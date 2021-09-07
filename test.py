4471c6f278fe7e07c1b53d182fb6e748d19f33bd
MaorUziel <63524728+MaorUziel@users.noreply.github.com>
2021-09-07 11:24:14 +0300

    Update test.py

--- test.py
+++ test.py
@@ -62,12 +62,11 @@ def get_service_gmail():
             flow = InstalledAppFlow.from_client_secrets_file(OAUTH2CLIENT_IDS_CREDS_PATH, SCOPES_GMAIL)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-	with open("/home/maor_animals_now_org/pytest/token.pickle", 'wb') as token:
+        with open("/home/maor_animals_now_org/pytest/token.pickle", 'wb') as token:
             pickle.dump(creds, token)
     service = build('gmail', 'v1', credentials=creds)
 
     return service
-
 
 service = get_service_gmail()
 message_text = "Just a small test"
