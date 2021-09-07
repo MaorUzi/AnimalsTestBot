11bb3d7662730ac07855eb998ee7b8f16a227045
MaorUziel <63524728+MaorUziel@users.noreply.github.com>
2021-09-07 10:13:55 +0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,29 @@
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import argparse
+
+parser.add_argument("--ServiceAccountCreds", default=["None"],
+					nargs=1, help="Please Enter Service Account creds path")
+
+
+SCOPES_SHEETS = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',
+                 "https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+
+
+SERVICE_ACCOUNTS_CREDS_PATH = args.ServiceAccountCreds[0]
+
+print(SERVICE_ACCOUNTS_CREDS_PATH)
+def get_service_sheet():
+    creds = ServiceAccountCredentials.from_json_keyfile_name(SERVICE_ACCOUNTS_CREDS_PATH, SCOPES_SHEETS)
+    client = gspread.authorize(creds)
+    return client
+  
+  
+  
+  
